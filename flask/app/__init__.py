814b4df4be53df888774d1eb34f4976927b5c9e0
ManhPham <manhterry93@gmail.com>
2021-01-11 09:29:08 +0000

    setup psql

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,4 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+#postgresql://[user]:[password]@[ip]/[dbname]
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres@192.168.1.200:5435/postgres' 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name=name
+        self.email=emails
+
+    def __repr__(self):
+        return '<User %r>' % self.username   
 
 from app import views
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,5 +1,17 @@
 from app import app
-
+from app import User
 @app.route("/")
 def index():
-    return ":Hello from Flask"
+    users = User.query.all()
+    if users is None:
+        return ":Hello from Flask"
+    else:
+        results = [
+        {
+            "name":user.name,
+            "id":user.id,
+            "email":user.email
+        }for user in users]
+        return {"count": len(results), "cars": results}
+    
+    
